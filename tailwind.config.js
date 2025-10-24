dffafd8619a1a346ff6dba60ca52cc92152cf2b6
Ouballa10 <abdelmounaimoub12@gmail.com>
2025-10-24 13:45:41 +0200

    final21s

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,14 @@ export default {
     extend: {
       colors: {
         primary: {
+          DEFAULT: '#31276C',
+          dark: '#241D52',
+          light: '#453A85',
+        },
+        secondary: {
           DEFAULT: '#FF8C00',
           dark: '#E67E00',
           light: '#FFA500',
-        },
-        secondary: {
-          DEFAULT: '#2C3E7E',
-          dark: '#1E2A5E',
-          light: '#3B4F9E',
         },
       },
     },
